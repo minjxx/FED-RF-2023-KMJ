088c02da44425e7952dbab5d1f300634973ffaa8
minjxx <qnsdbtlrp1018@naver.com>
2023-09-20 12:12:58 +0900

    up

--- 700.쉐도잉프로젝트/01.DKB PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB PJ/03.구현소스/js/main.js
@@ -1,77 +1,63 @@
 // 도깨비 PJ 메인 JS - main.js
 
-// DOM 함수 객체 //////////////
-const domFn = {
-    // 요소선택함수 ////////
-    qs: (x) => document.querySelector(x),
-    qsEl: (el, x) => el.querySelector(x),
-    qsa: (x) => document.querySelectorAll(x),
-    qsaEl: (el, x) => el.querySelectorAll(x),
+// 모듈 불러오기 ///////////////////
+// DOM함수 모듈
+import dFn from './dom.js';
+// 부드러운 스크롤 모듈
+import {startSS,setPos} from './smoothScroll23.js';
+// 데이터 모듈
+import { gridData,gnbData } from './data_drama.js';
 
-    // 이벤트셋팅함수
-    addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
-}; /////// domFn 객체 /////////////
+// 부드러운 스크롤 적용 //////////
+startSS();
 
-// 로딩구역 호출설정
-window.addEventListener("DOMContentLoaded", loadFn);
+// 부드러운 스크롤 때문에 마우스휠 이벤트막기가
+// 작동되어 캐릭터설명박스 작은 스크롤도 작동안됨!
+// 따라서 이벤트 버블링을 막아줘야함!
+// event.stopPropagation()
+// 이벤트 객체의 이벤트 버블링 막아주는 메서드임!
 
-//// 로딩구역 함수 /////////////////
-function loadFn() {
-    // 로딩확인
-    console.log("로딩완료!");
+// 대상: .desc-box
+let desc_box = document.querySelectorAll(".desc-box");
+console.log(desc_box);
 
-    // 부드러운 스크롤 적용 //////////
-    startSS();
+// 모든 캐릭터 설명박스는 이벤트 버블링 막기!!!
+// -> 여기서 마우스휠 됨!!!
+desc_box.forEach((ele) => {
+    // ele - 요소자신
+    ele.onwheel = (e) => e.stopPropagation();
+});
 
-    // 부드러운 스크롤 때문에 마우스휠 이벤트막기가
-    // 작동되어 캐릭터설명박스 작은 스크롤도 작동안됨!
-    // 따라서 이벤트 버블링을 막아줘야함!
-    // event.stopPropagation()
-    // 이벤트 객체의 이벤트 버블링 막아주는 메서드임!
+/**************************************** 
+     [ 현장포토파트 데이터 구성하기 ]
+    - 배열데이터를 이용하여 HTML코드 구성
+****************************************/
+// 1. 대상선정 : .live-box
+const liveBox = dFn.qs(".live-box");
+console.log("대상:", liveBox);
 
-    // 대상: .desc-box
-    let desc_box = document.querySelectorAll(".desc-box");
-    console.log(desc_box);
+// 2. 현장포토 데이터를 기반으로 HTML코드 만들기
+let hcode = "<ul>";
 
-    // 모든 캐릭터 설명박스는 이벤트 버블링 막기!!!
-    // -> 여기서 마우스휠 됨!!!
-    desc_box.forEach((ele) => {
-        // ele - 요소자신
-        ele.onwheel = (e) => e.stopPropagation();
-    });
+// 반복코드 만들기 /////
+// 현장포토 데이터 - data_drama.js에서 가져옴
+gridData[0].forEach((val) => {
+    // html변수에 계속 넣기
+    hcode += ` <li>
+            <figure>
+                <img src="images/live_photo/${val.imgName}.jpg" alt="${val.title}">
+                <figcaption>${val.title}</figcaption>
+            </figure>
+        </li>
+    `;
+}); //////// forEach /////////////
 
-    /**************************************** 
-      [ 현장포토파트 데이터 구성하기 ]
-      - 배열데이터를 이용하여 HTML코드 구성
-    ****************************************/
-    // 1. 대상선정 : .live-box
-    const liveBox = domFn.qs(".live-box");
-    console.log("대상:", liveBox);
+hcode += "</ul>";
 
-    // 2. 현장포토 데이터를 기반으로 HTML코드 만들기
-    let hcode = "<ul>";
+//   console.log(hcode);
 
-    // 반복코드 만들기 /////
-    // 현장포토 데이터 - data_drama.js에서 가져옴
-    liveData.forEach((val) => {
-        // html변수에 계속 넣기
-        hcode += ` <li>
-              <figure>
-                  <img src="images/live_photo/${val.imgName}.jpg" alt="${val.title}">
-                  <figcaption>${val.title}</figcaption>
-              </figure>
-          </li>
-        `;
-    }); //////// forEach /////////////
-
-    hcode += "</ul>";
-
-    //   console.log(hcode);
-
-    // 3. 대상박스에 html코드 넣기
-    liveBox.innerHTML = hcode;
-} ////////// loadFn함수 //////////////
-//////////////////////////////////////
+// 3. 대상박스에 html코드 넣기
+liveBox.innerHTML = hcode;
 
 //////////////////////////////////////////////
 // [ GNB 서브메뉴 셋팅하기 ]
@@ -80,13 +66,13 @@ function loadFn() {
 // 1.대상선정 : .gnb > ul > li
 // 서브메뉴 넣을 li는 하위 a요소의 텍스트가 gnbData속성명 1차 메뉴와
 // 일치하는 경우 하위 메뉴를 넣어준다!
-const gnbList = domFn.qsa(".gnb>ul>li");
+const gnbList = dFn.qsa(".gnb>ul>li");
 console.log("메뉴:", gnbList, "/ 데이터:", gnbData);
 
 // 3. 대상에 하위메뉴 태그 만들기
 gnbList.forEach((ele) => {
     // 1. 하위 a요소 텍스트 읽기
-    let atxt = domFn.qsEl(ele, "a").innerText;
+    let atxt = dFn.qsEl(ele, "a").innerText;
     // 2. GNB 데이터 읽기
     let gData = gnbData[atxt];
     // console.log('텍스트:',atxt,gData);
@@ -124,16 +110,16 @@ gnbList.forEach((ele) => {
   변경 대상: .smenu
 *************************************/
 // 1. 대상선정
-const gnb = domFn.qsa(".gnb>ul>li");
+const gnb = dFn.qsa(".gnb>ul>li");
 
 // 2. 이벤트 설정하기
 // 이벤트 종류: mouseover / mouseout
 gnb.forEach((ele) => {
     // 서브메뉴가 있을때만 이벤트 설정하기!
     // if문에서 undefined/null 은 false처리됨!
-    if(domFn.qsEl(ele,'.smenu')){
-        domFn.addEvt(ele, "mouseover", overFn);
-        domFn.addEvt(ele, "mouseout", outFn);
+    if(dFn.qsEl(ele,'.smenu')){
+        dFn.addEvt(ele, "mouseover", overFn);
+        dFn.addEvt(ele, "mouseout", outFn);
     }
 });
 
@@ -141,14 +127,14 @@ gnb.forEach((ele) => {
 function overFn() {
     // console.log('오버',this);
     // 1.하위 .smbx 높이값 알아오기
-    let hv = domFn.qsEl(this, ".smbx").clientHeight;
+    let hv = dFn.qsEl(this, ".smbx").clientHeight;
     // console.log('높이:',hv);
     // 2.하위 서브메뉴박스 만큼 .smenu 높이값 주기
-    domFn.qsEl(this, ".smenu").style.height = hv + "px";
+    dFn.qsEl(this, ".smenu").style.height = hv + "px";
 } //////////// overFn 함수 ////////////
 
 function outFn() {
     // console.log('아웃',this);
     // 서브메뉴 박스 높이값 0만들기!
-    domFn.qsEl(this, ".smenu").style.height = "0px";
+    dFn.qsEl(this, ".smenu").style.height = "0px";
 } //////////// outFn 함수 ////////////
