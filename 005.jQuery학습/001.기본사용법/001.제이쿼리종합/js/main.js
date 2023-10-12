a41beebb454a1cbaffd3c70ea7bc45d9f8f2e4a3
minjxx <qnsdbtlrp1018@naver.com>
2023-10-12 11:34:31 +0900

    update

--- 005.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
+++ 005.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
@@ -3,8 +3,8 @@
 /*********************************** 
     [ 요구사항정리 ]
     1. 버튼을 클릭하여 미니언즈가
-    순서대로 특정위치로 이동하며
-    메시지를 보여준다
+        순서대로 특정위치로 이동하며
+        메시지를 보여준다
     2. 각 위치별 좀비를 등장시킨다
     3. 맨윗층에서 탈출할때 헬기를 사용한다
 
@@ -23,22 +23,46 @@
 
 // 0. 주인공들 변수에 할당!
 // (1) 미니언즈
-const mi = $('.mi');
+const mi = $(".mi");
 
 // (2) 건물 li
-const room = $('.building li');
+const room = $(".building li");
 
 // (3) 버튼들
-const btns = $('.btns button');
+const btns = $(".btns button");
 
 // (4) 메시지 박스
-const msg = $('.msg');
+const msg = $(".msg");
 
 // (5) 좀비, 주사기 요소 변수처리
 let mz1 = `<img src="./images/mz1.png" alt="좀비1" class="mz">`;
 let mz2 = `<img src="./images/mz2.png" alt="좀비2" class="mz">`;
 let zom = `<img src="./images/zom.png" alt="좀비들" class="mz">`;
 let inj = `<img src="./images/inj.png" alt="주사기" class="inj">`;
+
+// (6) 메시지 배열셋팅
+const msgTxt = [
+    // 0번방
+    "",
+    // 1번방
+    "",
+    // 2번방
+    "",
+    // 3번방
+    "",
+    // 4번방
+    "",
+    // 5번방
+    "",
+    // 6번방
+    "",
+    // 7번방
+    "",
+    // 8번방
+    `와~! 아늑하다!<br>옆방으로 가보자!`,
+    // 9번방
+    "악!;;;; 좀비!<br>어서피하자!",
+];
 
 // console.log('대상:',mi,room,btns,msg);
 
@@ -48,74 +72,135 @@ let inj = `<img src="./images/inj.png" alt="주사기" class="inj">`;
 // (1) each((순번,요소)=>{}) : 요소의 개수만큼 순서대로 돌아줌!
 // (2) append(요소) : 선택요소 내부에 자식요소 추가(이동)
 
-room.each((idx,ele)=>{
+room.each((idx, ele) => {
     // console.log(idx,ele);
     // 1. 각 방에 숫자로 순번넣기
     $(ele).text(idx);
 
     // 2. 좀비/주사기 넣기
-    switch(idx){
+    switch (idx) {
         case 9:
             $(ele).append(mz1);
-            break; // break; 이걸 안해주면 다른 case와 엉킴. 
+            break;
         case 7:
             $(ele).append(mz2);
-            break; // break; 이걸 안해주면 다른 case와 엉킴. 
+            break;
         case 2:
             $(ele).append(inj);
-            break; // break; 이걸 안해주면 다른 case와 엉킴. 
+            break;
         case 1:
             $(ele).append(zom);
-            break; // break; 이걸 안해주면 다른 case와 엉킴. 
-        
-    } ///// switch case //////
-}); /////////// each 메서드 /////////////
+            break;
+    } /////// switch case //////
+}); /////////// each 메서드 //////////////
 
 // 좀비는 모두 숨기기
-$('.mz').hide();
+$(".mz").hide();
 // 시간이 없는 hide()는 display:none 처리함!
 
-// 2. 버튼 셋팅하기 //////////////////////
+// 2. 버튼 셋팅하기 //////////////////
 // 대상: .btns button -> btns변수
 btns.hide().first().show();
-// btns.hide().eq(0).show();
+// 버튼들.숨겨().첫번째().보여()
+// btns.hide().eq(4).show();
 
-// 3. 미니언즈 공통 기능함수 ///////////
+// 3. 미니언즈 공통 기능함수 /////////
 // (1) ele - 클릭된 버튼요소
-// (2) seq = 이동할 li방 순번
+// (2) seq - 이동할 li방 순번
 // (3) fn - 이동후 실행할 코드(콜백함수)
 const actMini = (ele, seq, fn) => {
-    
-}; ///////// actMini함수 ///////////
-
-// 4. "들어가기" 버튼 클릭시 ////////////////
-btns.first().click(function(){
-    // 0. 메시지 숨기기
+    // 0. 메시지 숨기기 + 버튼 숨기기
     msg.fadeOut(300);
+    // this는 클릭된 버튼자신! -> ele로 전달!
+    $(ele).slideUp(400);
 
-    // 1. 위치값 읽기
+    // 1. 위치값 읽기 : seq에 방번호를 보냄!
     // 원리: 이동할 li방 위치값을 읽은 후 이동함
-    let myRoom = room.eq(8);
+    let myRoom = room.eq(seq);
     // 위치값 배열변수
     let pos = [];
-    // top위치값
+    // top 위치값
     pos[0] = myRoom.offset().top;
-    // left위치값
-    pos[1] = myRoom.offset().left;
+    // left 위치값 : 방에서 중앙에 위치하도록 보정
+    // -> left값 + 방width절반 - 미니언즈width절반
+    pos[1] = myRoom.offset().left + myRoom.width() / 2 - mi.width() / 2;
 
-    console.log('위치값:',pos[0],pos[1]);
-    
+    // 제이쿼리 위치값 정보 메서드 :  offset()
+    // ->하위속성: offset().top / offset().left
+    // 제이쿼리 가로,세로 크기정보 메서드 :
+    // -> 가로크기 width() / 세로크기 height()
+
+    console.log("위치값:", pos[0], "/", pos[1]);
+
     // 2. 이동하기
     // 대상: .mi -> mi변수
     // animate({CSS설정},시간,이징,콜백함수)
-    mi.animate({
-        top:pos[0] + 'px',
-        left:pos[1] + 'px'
-    },800,"easeOutElastic",()=>{
-        // 메시지 변경
-        msg.html(`와~! 아늑하다<br>옆방으로 가보자!`).delay(1000).fadeIn(300);
-        // 메시지 보이기
-    }); ////// animate //////
+    mi.animate(
+        {
+            top: pos[0] + "px",
+            left: pos[1] + "px",
+        },
+        800,
+        "easeOutElastic",
+        // 콜백함수
+        fn
+    ); ///////// animate ////////
+}; /////////// actMini함수 //////////////
 
+// 다음버튼 보이기 함수 ///////////////////////////
+const showNextBtn = (ele) => 
+$(ele).next().delay(1000).slideDown(400);
+////////////// showNextBtn함수 //////////////
 
-}); //// "들어가기" 버튼 끝 ////////
+// 4. "들어가기" 버튼 클릭시 /////////////
+btns.first() // 첫번째버튼
+    .click(function () {
+        // 하위 이벤트함수 this의미!
+        // ()=>{
+
+        // 버튼별 콜백함수 만들기 ////////
+        let fn =
+            // function(){ -> this가 mi임!
+            () => {
+                // this가 싸고있는 버튼요소임!
+
+                // 메시지변경 + 메시지 보이기
+                msg.html(msgTxt[8]).delay(1000).fadeIn(300);
+
+                // console.log('미니언즈 콜백함수:',this);
+                // 다음버튼 보이기
+                showNextBtn(this);
+            }; ////////// 콜백함수 /////////////
+
+        // 미니언즈 공통함수 호출
+        actMini(this, 8, fn);
+    }) //// "들어가기" 버튼 끝 //////////
+
+    // 5. "옆방으로!" 버튼 클릭시 /////////////
+    // 위의 버튼에서 이어짐!
+    .next() // 두번째버튼
+    .click(function () {
+        // 하위 이벤트함수 this의미!
+        // ()=>{
+
+        // 버튼별 콜백함수 만들기 ////////
+        let fn =
+            // function(){ -> this가 mi임!
+            () => {
+                // 좀비 나타나기(2초후)
+                room.eq(9).find('.mz')
+                .delay(2000)
+                .fadeIn(400,()=>{
+                    // 콜백함수
+                    // 메시지 보이기
+                    msg.html(msgTxt[9])
+                    .css({left:"-89%"})
+                    .fadeIn(300);
+                    // 다음버튼 보이기
+                    showNextBtn(this);
+                }); /////// fadeIn /////////
+            }; ////////// 콜백함수 /////////////
+
+        // 미니언즈 공통함수 호출
+        actMini(this, 9, fn);
+    }); //// "옆방으로!" 버튼 끝 //////////
