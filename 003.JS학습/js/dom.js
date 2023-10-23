0f4047babfde96454390108f541652050302d702
minjxx <qnsdbtlrp1018@naver.com>
2023-10-23 17:36:42 +0900

    update

--- 003.JS학습/js/dom.js
+++ 003.JS학습/js/dom.js
@@ -9,13 +9,13 @@ export default {
     qsEl: (el, x) => el.querySelector(x),
     qsa: (x) => document.querySelectorAll(x),
     qsaEl: (el, x) => el.querySelectorAll(x),
-
+  
     // 이벤트셋팅함수
     addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
-
+  
     // 바운딩위치값함수
     getBCR: (ele) => ele.getBoundingClientRect().top,
-
+  
     // 옵셋탑값 반환함수
     getOT: (ele) => ele.offsetTop,
-}; /////// domFn 객체 /////////////
+  }; /////// domFn 객체 /////////////
