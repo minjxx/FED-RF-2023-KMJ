9237c225412baad32cf10d80344a50adbed5c905
minjxx <qnsdbtlrp1018@naver.com>
2023-10-25 12:37:26 +0900

    update

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/linksys.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/linksys.js
@@ -1,16 +1,16 @@
 // 보그 PJ 링크 시스템 JS - linksys.js
 
-// 제이쿼리 로드 구역 //////
+// 제이쿼리 로드 구역 ///////////
 $(()=>{
-    
+
     // 모든 a요소 기본이동막기
     $('a').click(e=>e.preventDefault());
 
     // 요구사항: 각 네비게이션 클릭시 페이지이동하기
-    // 1. 대상선정
-    // 1-1. 로고링크 :  .logo a
+    // 1. 대상선정 
+    // 1-1. 로고링크 : .logo a
     const logo = $('.logo a');
-    // 1-1. GNB 메뉴 : .gnb a
+    // 1-2. GNB 메뉴 : .gnb a
     const gnb = $('.gnb a');
     // console.log('대상:',logo,gnb);
 
@@ -19,12 +19,17 @@ $(()=>{
     logo.click(()=>location.href='index.html');
 
     // 2-2. gnb 메뉴 클릭시 카테고리 서브 이동
+    gnb.click(e=>
+        location.href='category.html?cat='+
+        $(e.target).text().toLowerCase());
+        // console.log($(e.target).text().toLowerCase()))
+
     // e.target -> 이벤트발생요소(a요소)
-    // text() -> a요소 텍스트
+    // text() -> a요소 텍스트읽기
     // toLowerCase() -> 소문자전환
     // 참고) toUpperCase() -> 대문자전환
 
-    gnb.click(e=> 
-        location.href='category.html?cat='+$(e.target).text().toLowerCase());
-        // console.log($(e.target).text().toLowerCase()))
-}); /////////// JQB /////////////
+
+
+
+}); //////////// jQB ///////////////////
