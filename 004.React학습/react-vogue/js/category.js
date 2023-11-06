016ef154821d5e873033059ebba364f2239cea7f
minjxx <qnsdbtlrp1018@naver.com>
2023-11-06 15:01:51 +0900

    update

--- 004.React학습/react-vogue/js/category.js
+++ 004.React학습/react-vogue/js/category.js
@@ -14,96 +14,133 @@ import catData from "./data/category_data.js";
   기능 : 상단영역 메뉴, 로그 등 요소 구성
 *******************************************/
 function TopArea() {
+    // 컴포넌트 요소 랜더링 직전 호출구역
+    // -> 컴포넌트는 모두 만들어진 후 화면뿌리기 직전(가랜더랭)
+    React.useLayoutEffect(makeLink);
 
-  // 컴포넌트 요소 랜더링 직전 호출구역
-  // -> 컴포넌트는 모두 만들어진 후 화면뿌리기 직전(가랜더랭)
-  React.useLayoutEffect(makeLink);
+    // GNB용 메뉴 배열변수
+    const gnbText = ["FASHION", "BEAUTY", "LIVING", "PEOPLE", "VIDEO", "RUNWAY", "TIME & GEM", "SHOPPING"];
 
-  // GNB용 메뉴 배열변수
-  const gnbText = ["FASHION","BEAUTY","LIVING","PEOPLE","VIDEO","RUNWAY","TIME & GEM","SHOPPING"];
+    // 메뉴 클릭시 변수 변경함수
+    const chgCat = (data) => {
+        console.log("나야나!", data);
+    }; ///////////// chgCat 함수 ///////////
 
-  // 메뉴 클릭시 변수 변경함수
-  const chgCat = data => {
-    console.log('나야나!',data);
-  }; ///////////// chgCat 함수 ///////////
+    return (
+        <React.Fragment>
+            {/* 1-1.상단메뉴 */}
+            <div className="tmenu">
+                {/* 1-1-1.sns박스 */}
+                <div className="sns">
+                    <a href="#" className="fi fi-instagram">
+                        <span className="ir">인스타그램</span>
+                    </a>
+                    <a href="#" className="fi fi-facebook">
+                        <span className="ir">페이스북</span>
+                    </a>
+                    <a href="#" className="fi fi-twitter">
+                        <span className="ir">트위터</span>
+                    </a>
+                    <a href="#" className="fi fi-youtube-play">
+                        <span className="ir">유튜브</span>
+                    </a>
+                    <a href="#" className="fi cas">
+                        <span className="ir">카카오스토리</span>
+                    </a>
+                </div>
+                {/* 1-1-2.사이드메뉴 */}
+                <div className="sideMenu">
+                    <ul className="smbx">
+                        <li>
+                            <a href="#">SIDE MENU</a>
+                            {/* 서브메뉴 */}
+                            <ol className="smsub">
+                                <li>
+                                    <a href="#">회사 소개</a>
+                                </li>
+                                <li>
+                                    <a href="#">광고 및 제휴</a>
+                                </li>
+                                <li>
+                                    <a href="#">개인정보 처리방침</a>
+                                </li>
+                            </ol>
+                        </li>
+                        <li>
+                            <a href="#">SUBSCRIBE</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            {/* 1-2.로고박스 */}
+            <h1 className="logo">
+                <a href="#">
+                    <img src="./images/mlogo.png" alt="메인로고" />
+                </a>
+            </h1>
+            {/* 1-3.GNB박스 */}
+            <nav className="gnb">
+                <ul>
+                    {gnbText.map((v) => (
+                        <li>
+                            <a href="#" onClick={() => chgCat(v)}>
+                                {v}
+                            </a>
+                        </li>
+                    ))}
 
-  return (
-    <React.Fragment>
-      {/* 1-1.상단메뉴 */}
-      <div className="tmenu">
-        {/* 1-1-1.sns박스 */}
-        <div className="sns">
-          <a href="#" className="fi fi-instagram">
-            <span className="ir">인스타그램</span>
-          </a>
-          <a href="#" className="fi fi-facebook">
-            <span className="ir">페이스북</span>
-          </a>
-          <a href="#" className="fi fi-twitter">
-            <span className="ir">트위터</span>
-          </a>
-          <a href="#" className="fi fi-youtube-play">
-            <span className="ir">유튜브</span>
-          </a>
-          <a href="#" className="fi cas">
-            <span className="ir">카카오스토리</span>
-          </a>
-        </div>
-        {/* 1-1-2.사이드메뉴 */}
-        <div className="sideMenu">
-          <ul className="smbx">
-            <li>
-              <a href="#">SIDE MENU</a>
-              {/* 서브메뉴 */}
-              <ol className="smsub">
-                <li>
-                  <a href="#">회사 소개</a>
-                </li>
-                <li>
-                  <a href="#">광고 및 제휴</a>
-                </li>
-                <li>
-                  <a href="#">개인정보 처리방침</a>
-                </li>
-              </ol>
-            </li>
-            <li>
-              <a href="#">SUBSCRIBE</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      {/* 1-2.로고박스 */}
-      <h1 className="logo">
-        <a href="#">
-          <img src="./images/mlogo.png" alt="메인로고" />
-        </a>
-      </h1>
-      {/* 1-3.GNB박스 */}
-      <nav className="gnb">
-        <ul>
-          {
-            gnbText.map(v=>
-              <li>
-                <a href="#" onClick={()=>chgCat(v)}>{v}</a>
-              </li>)
-          }
-          
-          <li>
-            {/* 돋보기 검색버튼 */}
-            <i href="#" className="fi fi-search">
-              <span className="ir">search</span>
-            </i>
-          </li>
-        </ul>
-      </nav>
-    </React.Fragment>
-  );
+                    <li>
+                        {/* 돋보기 검색버튼 */}
+                        <i href="#" className="fi fi-search">
+                            <span className="ir">search</span>
+                        </i>
+                    </li>
+                </ul>
+            </nav>
+        </React.Fragment>
+    );
 } //////////// TopArea 컴포넌트 /////////////
 
 // 상단영역 출력하기 /////////////////
-ReactDOM.render(<TopArea />, 
-document.querySelector(".top-area"));
+ReactDOM.render(<TopArea />, document.querySelector(".top-area"));
+/////////////////////////////////////
+
+/////////// 하단영역 컴포넌트 /////////////
+/******************************************* 
+  컴포넌트명 : FooterArea
+  기능 : 하단영역 링크메뉴, 회사정보
+*******************************************/
+function FooterArea() {
+
+    // 하단링크 데이터 
+    const fTxt = ["정기구독","회사소개","광고 및 제휴","개인정보 처리방침"];
+
+    const makeList = (data) => data.map((v)=>(
+        <li>
+            <a href="#">{v}</a>
+        </li>
+    ));
+
+    return(
+        <React.Fragment>
+        {/* 3-1.하단로고 */}
+        <div className="blogo">
+            <img src="./images/footer_logo.png" alt="하단로고" />
+        </div>
+        {/* 3-2.회사주소 */}
+        <address className="addr">
+            WWW.VOGUE.COM Ⓒ CONDÉNAST ASIA PACIFIC. INC. 
+            ALL RIGHTS RESERVED. VOGUE.COM KOREA IS OPERATED 
+            BY DOOSAN MAGAZINE.
+        </address>
+        {/* 3-3.하단링크 */}
+        <ul className="blink">{makeList(fTxt)}</ul>
+        </React.Fragment>
+    );
+} //////////// FooterArea 컴포넌트 /////////////
+
+// 하단영역 출력하기 /////////////////
+ReactDOM.render(<FooterArea />, document.querySelector(".footer-area"));
 /////////////////////////////////////
 
 //////////////////////////////////////////////////////
@@ -114,52 +151,40 @@ document.querySelector(".top-area"));
   기능 : 아이템 페이지 타이틀 + 리스트 요소구성
 *******************************************/
 function MainCategory() {
+    // 우선 URL로 넘어온 키값을 가져옴!
+    // 파라미터 전달값 받기 : 파라미터JS전담객체는?
+    // -> URLSearchParams(전체URL)
+    const params = new URLSearchParams(location.search);
 
-  // 우선 URL로 넘어온 키값을 가져옴!
-  // 파라미터 전달값 받기 : 파라미터JS전담객체는?
-  // -> URLSearchParams(전체URL)
-  const params = new URLSearchParams(location.search);
+    // 파라미터중 특정키 받기 : get(키이름) -> 키이름은 'cat'
+    const catName = decodeURIComponent(params.get("cat"));
+    // 'time & gem' decodeURIComponent로 변환!
+    // -> 보내는 곳에서는 encodeURIComponent로 처리해야함!
 
-  // 파라미터중 특정키 받기 : get(키이름) -> 키이름은 'cat'
-  const catName = decodeURIComponent(params.get('cat'));
-  // 'time & gem' decodeURIComponent로 변환!
-  // -> 보내는 곳에서는 encodeURIComponent로 처리해야함!
+    console.log("URL", location.search, "\n파라미터:", params, "\n키값:", catName);
 
-  console.log(
-    'URL',location.search,
-    '\n파라미터:',params,
-    '\n키값:',catName);
+    // 카테고리 데이터 상태관리변수 만들기!
+    const [nowCat, setNowCat] = React.useState(catName);
 
-  // 카테고리 데이터 상태관리변수 만들기!
-  const [nowCat,setNowCat] = React.useState(catName);
+    // 카테고리 해당 데이터 선택하기
+    // 카테고리 전체 객체 데이터 중 해당항목 선택
+    const selData = catData[nowCat];
 
+    console.log(selData);
 
-  // 카테고리 해당 데이터 선택하기
-  // 카테고리 전체 객체 데이터 중 해당항목 선택
-  const selData = catData[nowCat];
+    const chgMenu = () => setNowCat("living");
 
-  console.log(selData);
-
-  const chgMenu = () => setNowCat('living');
-
-  return(
-    <React.Fragment>
-      <SubTitle 
-        tit={selData['제목']} 
-        menu={selData['메뉴']}
-        />
-      <button onClick={chgMenu}>변경해!</button>
-      <ItemList 
-        cname={selData['경로']} 
-        tit={selData['타이틀']} />
-    </React.Fragment>
-  );
-
+    return (
+        <React.Fragment>
+            <SubTitle tit={selData["제목"]} menu={selData["메뉴"]} />
+            <button onClick={chgMenu}>변경해!</button>
+            <ItemList cname={selData["경로"]} tit={selData["타이틀"]} />
+        </React.Fragment>
+    );
 } ///////// MainCategory 컴포넌트 /////////////
 
 // 메인영역 출력하기 ////////////////////
-ReactDOM.render(<MainCategory />, 
-document.querySelector(".main-area"));
+ReactDOM.render(<MainCategory />, document.querySelector(".main-area"));
 ///////////////////////////////////////
 
 ////// 메인 컴포넌트 하위 서브타이틀 컴포넌트 /////
@@ -167,42 +192,38 @@ document.querySelector(".main-area"));
   컴포넌트명 : SubTitle
   기능 : 서브 타이틀 요소구성
 *******************************************/
-function SubTitle(props){ 
-  // tit - 서브타이틀 / menu - 서브메뉴
+function SubTitle(props) {
+    // tit - 서브타이틀 / menu - 서브메뉴
 
-  // 서브메뉴 있을경우 li데이터 생성하기
-  // 전달변수 data에 들어오는 값은 메뉴 배열임!
-  // 배열.map(v=>코드) -> html코드 생성후 리턴됨!
-  const makeList = (data) => data.map(v=>
-      <li>
-        <a href="#">{v}</a>
-      </li>
-    ); ///////// makeList 함수 ////////
+    // 서브메뉴 있을경우 li데이터 생성하기
+    // 전달변수 data에 들어오는 값은 메뉴 배열임!
+    // 배열.map(v=>코드) -> html코드 생성후 리턴됨!
+    const makeList = (data) =>
+        data.map((v) => (
+            <li>
+                <a href="#">{v}</a>
+            </li>
+        )); ///////// makeList 함수 ////////
 
     // -> 오리지널 JS map()문법은 배열을 다시 리턴함
     // JS에서는 배열.map().join('') 로 사용했음
     // -> 리액트에서는 리액트용 map()을 다시 구성하여
     // 바로 html코드를 리턴함! join() 불필요!!!
 
-  return(
+    return (
         // 2-1. 카테고리 페이지 상단영역
         <header className="cat-top-area">
-          {/* 2-1-1. 서브타이틀 */}
-          <h2 className="cat-tit">{props.tit}</h2>
-          {/* 2-1-2. 서브메뉴(LNB:Local Navigation Bar)
+            {/* 2-1-1. 서브타이틀 */}
+            <h2 className="cat-tit">{props.tit}</h2>
+            {/* 2-1-2. 서브메뉴(LNB:Local Navigation Bar)
           -> 메뉴 데이터 값이 '없음'이 아닐때만 생성됨! */}
-          {
-            props.menu!='없음' &&
-            <nav className="lnb">
-              <ul>
-                {makeList(props.menu)}
-              </ul>
-            </nav>
-
-          }
+            {props.menu != "없음" && (
+                <nav className="lnb">
+                    <ul>{makeList(props.menu)}</ul>
+                </nav>
+            )}
         </header>
-  );
-
+    );
 } /////////////// SubTitle 컴포넌트 //////////////
 
 ////// 메인 컴포넌트 하위 리스트 컴포넌트 /////
@@ -210,62 +231,52 @@ function SubTitle(props){
   컴포넌트명 : ItemList
   기능 : 카테고리 아이템별 리스트요소구성
 *******************************************/
-function ItemList(props){
-  // cname - 카테고리명(클래스명넣기)
-  // tit - 리스트 타이틀
+function ItemList(props) {
+    // cname - 카테고리명(클래스명넣기)
+    // tit - 리스트 타이틀
 
-  // 태그처리 구분 코드생성 함수
-  const makeCode = data => {
-    console.log('배열인가?',Array.isArray(data));
-    // 배열데이터인 경우 ////
-    if(Array.isArray(data)){ 
-        return( 
-          <h2>
-              <small>{data[0]}</small><br/>{data[1]}
-          </h2>
-        )
-    } /////// if ////////
-    else{ // 배열이 아닌경우 ////
-      return(<h2>{data}</h2>);
-    } /////// else ///////
-  }; ////////// makeCode함수 ////////////
+    // 태그처리 구분 코드생성 함수
+    const makeCode = (data) => {
+        console.log("배열인가?", Array.isArray(data));
+        // 배열데이터인 경우 ////
+        if (Array.isArray(data)) {
+            return (
+                <h2>
+                    <small>{data[0]}</small>
+                    <br />
+                    {data[1]}
+                </h2>
+            );
+        } /////// if ////////
+        else {
+            // 배열이 아닌경우 ////
+            return <h2>{data}</h2>;
+        } /////// else ///////
+    }; ////////// makeCode함수 ////////////
 
-  return(
-    // 2-2. 카테고리 페이지 컨텐츠영역
-    // html출력일 경우 dangerouslySetInnerHTML을 사용함!
-    // <요소 dangerouslySetInnerHTML={{__html:값}}>
-    // <h2 dangerouslySetInnerHTML={{__html:props.tit[0]}}>
-    // -> script태그와 같은 위험요소가 실행될 수 있으므로
-    // 데이터에서 직접적으로 태그를 넣는 방법을 비추천함!
-    // -> 개선: 태그 데이터를 제거하고 데이터화 함!
-    // -> runway 데이터를 배열형으로 만들고 첫번째 데이터를
-    // small태그로 싸고 뒤에 br태그로 줄바꿈한 구성을 
-    // 컴포넌트에서 구성하도록 변경함!
+    return (
+        // 2-2. 카테고리 페이지 컨텐츠영역
+        // html출력일 경우 dangerouslySetInnerHTML을 사용함!
+        // <요소 dangerouslySetInnerHTML={{__html:값}}>
+        // <h2 dangerouslySetInnerHTML={{__html:props.tit[0]}}>
+        // -> script태그와 같은 위험요소가 실행될 수 있으므로
+        // 데이터에서 직접적으로 태그를 넣는 방법을 비추천함!
+        // -> 개선: 태그 데이터를 제거하고 데이터화 함!
+        // -> runway 데이터를 배열형으로 만들고 첫번째 데이터를
+        // small태그로 싸고 뒤에 br태그로 줄바꿈한 구성을
+        // 컴포넌트에서 구성하도록 변경함!
 
-    <div className={"cat-cont-area "+props.cname}>
-      <section className="pt2">
-        <div className="cbx bgi bg1-1">
-            {makeCode(props.tit[0])}
+        <div className={"cat-cont-area " + props.cname}>
+            <section className="pt2">
+                <div className="cbx bgi bg1-1">{makeCode(props.tit[0])}</div>
+                <div className="cbx bgi bg1-2">{makeCode(props.tit[1])}</div>
+                <div className="cbx bgi bg1-3">{makeCode(props.tit[2])}</div>
+            </section>
+            <section className="pt2">
+                <div className="cbx bgi bg2-1">{makeCode(props.tit[3])}</div>
+                <div className="cbx bgi bg2-2">{makeCode(props.tit[4])}</div>
+                <div className="cbx bgi bg2-3">{makeCode(props.tit[5])}</div>
+            </section>
         </div>
-        <div className="cbx bgi bg1-2">
-            {makeCode(props.tit[1])}
-        </div>
-        <div className="cbx bgi bg1-3">
-            {makeCode(props.tit[2])}
-        </div>
-      </section>
-      <section className="pt2">
-        <div className="cbx bgi bg2-1">
-            {makeCode(props.tit[3])}
-        </div>
-        <div className="cbx bgi bg2-2">
-            {makeCode(props.tit[4])}
-        </div>
-        <div className="cbx bgi bg2-3">
-            {makeCode(props.tit[5])}
-        </div>
-      </section>
-    </div>
-  );
-
+    );
 } /////////////// ItemList 컴포넌트 //////////////
