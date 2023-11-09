a3e00189d1d9381a21bd51c1b2ffa3e64fa6cbf7
minjxx <qnsdbtlrp1018@naver.com>
2023-11-09 17:35:11 +0900

    Update MainArea.jsx

--- 004.React학습/react-app/kmj-app/my-frist-react-app/src/dc/layout/MainArea.jsx
+++ 004.React학습/react-app/kmj-app/my-frist-react-app/src/dc/layout/MainArea.jsx
@@ -1,6 +1,6 @@
 // MainArea 컴포넌트
 
-import { Character } from "../contents/Charactor";
+import { Character } from "../contents/Character";
 import { Comics } from "../contents/Comics";
 import { Games } from "../contents/Games";
 import { Main } from "../contents/Main";
@@ -8,25 +8,25 @@ import { Movies } from "../contents/Movies";
 import { News } from "../contents/News";
 import { Video } from "../contents/Video";
 
-export function MainArea(props) {
-  // cat 속성으로 메뉴분류 전달
+export function MainArea(props){
+    // cat 속성으로 메뉴분류 전달
 
-  return (
-    <main className="cont">
-      {props.cat == "main" && 
-      <Main cat={props.cat} />}
-      {props.cat == "CHARACTERS" && 
-      <Character cat={props.cat} />}
-      {props.cat == "COMICS" && 
-      <Comics cat={props.cat} />}
-      {props.cat == "MOVIES" && 
-      <Movies cat={props.cat} />}
-      {props.cat == "GAMES" && 
-      <Games cat={props.cat} />}
-      {props.cat == "NEWS" && 
-      <News cat={props.cat} />}
-      {props.cat == "VIDEO" && 
-      <Video />}
-    </main>
-  );
-} ///////// MainArea 컴포넌트 ////////////
+    return(
+        <main className="cont">
+            {props.cat=='main' &&
+            <Main cat={props.cat} />}
+            {props.cat=='CHARACTERS' &&
+            <Character cat={props.cat} />}
+            {props.cat=='COMICS' &&
+            <Comics cat={props.cat} />}
+            {props.cat=='MOVIES' &&
+            <Movies cat={props.cat} />}
+            {props.cat=='GAMES' &&
+            <Games cat={props.cat} />}
+            {props.cat=='NEWS' &&
+            <News cat={props.cat} />}
+            {props.cat=='VIDEO' &&
+            <Video />}
+        </main>
+    );
+} /////////// MainArea 컴포넌트 /////////////
