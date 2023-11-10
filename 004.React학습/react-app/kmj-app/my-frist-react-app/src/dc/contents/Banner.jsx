e614e0c8020625e6ea8af28a5329a19edd8e53e4
minjxx <qnsdbtlrp1018@naver.com>
2023-11-10 12:22:41 +0900

    Update Banner.jsx

--- 004.React학습/react-app/kmj-app/my-frist-react-app/src/dc/contents/Banner.jsx
+++ 004.React학습/react-app/kmj-app/my-frist-react-app/src/dc/contents/Banner.jsx
@@ -14,10 +14,32 @@ import "jquery-ui-dist/jquery-ui";
 function slideFn() {
   // 1. 대상선정
   const sldBox = $(".slider");
+  // (6) 슬라이드 블릿
+  const indic = $('.indic li');
+  // console.log('블릿:',indic);
+
+  // 2. 변수설정
+  // (1) 애니시간
+  const A_TM = 600;
+  // (2) 애니이징
+  const A_ES = "easeInOutQuint";
+  // (3) 광클상태변수(1-불허용,0-허용)
+  let cSts = 0; // 0일때 false & 1일때 true
+  // (4) 슬라이드순번
+  let sNum = 0;
+  // (5) 슬라이드개수
+  const sCnt = sldBox.find('li').length;
+  // console.log('슬라이드개수:',sCnt);
 
   // 2. 이벤트설정 및 기능구현
   // 이동버튼 클릭시
   $(".abtn").click(function () {
+    // 0. 광클금지 ////////////////////
+    if(cSts) return; // 1) 처음에 0값 들어오면 fasle니까 반환되서 아래 문장실행
+    cSts=1;//잠금 // 2) cSts값 1로 지정
+    setTimeout(()=>cSts=0,A_TM); // 3) 0.6초 뒤에 cSts값 0으로 만들어줌, 0.6초안에는 계속 1 값으로 됨, 1은 true값이니까 return 안됨
+    /////////////////////////////////
+
     // 1. 오른쪽버튼 여부
     let isR = $(this).is(".rb");
     console.log("버튼클릭!", isR);
@@ -27,26 +49,51 @@ function slideFn() {
     if (isR) {
       // 슬라이드가 왼쪽으로 이동함
       // left값이 -100%
-      sldBox.animate({ left: "-100%" }, 600, "easeInOutQuint", 
+      sldBox.animate({ left: "-100%" }, A_TM, A_ES, 
       () => { // 콜백함수(애니후)
         // 맨앞li 맨뒤로 이동
         sldBox.append(sldBox.find('li').first())
         // 동시에 left값은 0으로 초기화함!
         .css({left:'0'});
       });
+      // 슬라이드 순번 증가(끝번호보다 크면 0)
+      sNum++;
+      if(sNum>=sCnt) sNum = 0;
     } //////// if /////////
+    // 2-2왼쪽버튼
+    else{
+      // 맨뒤li 맨앞으로 이동
+      sldBox.prepend(sldBox.find('li').last())
+      // left값 -100%
+      .css({left:'-100%'})
+      // left값을 0으로 애니메이션
+      .animate({left:'0'},A_TM,A_ES);
+      // 슬라이드 순번 감소(0보다 작으면 끝번호)
+      sNum--;
+      if(sNum<0) sNum = sCnt-1;
+    } /////// else ////////
+
+    // console.log('슬순번:',sNum);
+
+    // 블릿해당순번 클래스'on'넣기(다른li는 제거)
+    indic.eq(sNum).addClass('on')
+    .siblings().removeClass('on');
+
   }); /////////// click //////////
 } //////////// slideFn 함수 ///////////
 
 // 배너 컴포넌트 //
 export function Banner(props) {
   // category - 카테고리 분류명(베너 데이터선택)
+  
+  // 선택데이터
+  const selData = banData[props.category];
 
   // 페이지 랜더링후 실행구역
   useEffect(() => {
     console.log("랜더링후~!");
-    // 슬라이드 기능구현함수 호출
-    slideFn();
+    // 슬라이드 기능구현함수 호출 : 선택데이터가 1초과일때
+    if(selData.length>1) slideFn();
   }); ///////// useEffect //////////
 
   // 리스트만들기 함수
@@ -58,9 +105,6 @@ export function Banner(props) {
       </li>
     ));
   };
-
-  // 선택데이터
-  const selData = banData[props.category];
 
   // 코드리턴 ////////////////
   return (
