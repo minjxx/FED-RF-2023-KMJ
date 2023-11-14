5b2812ef899737b1b885b15f2f0be810ce05acb5
minjxx <qnsdbtlrp1018@naver.com>
2023-11-14 10:42:03 +0900

    update

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/layout/MainArea.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/layout/MainArea.jsx
@@ -6,14 +6,14 @@ import { StyleSub } from "../pages/StyleSub";
 import { WomenSub } from "../pages/WomenSub";
 
 export function MainArea(props){
-  // props.page 속성값으로 main/men/women/style
-  return(
-    <>
-      <h1>메인영역</h1>
-      {props.page=='main'&&<MainCont />}
-      {props.page=='men'&&<MenSub />}
-      {props.page=='women'&&<WomenSub />}
-      {props.page=='style'&&<StyleSub />}
-    </>
-  );
-} //////////// MainArea 컴포넌트 ////////////
+    // props.page 속성값으로 main/men/women/style
+    return(
+        <>
+            <h1>메인영역</h1>
+            {props.page=='main'&&<MainCont />}
+            {props.page=='men'&&<MenSub />}
+            {props.page=='women'&&<WomenSub />}
+            {props.page=='style'&&<StyleSub />}
+        </>
+    );
+} //////////////// MainArea 컴포넌트 //////////
