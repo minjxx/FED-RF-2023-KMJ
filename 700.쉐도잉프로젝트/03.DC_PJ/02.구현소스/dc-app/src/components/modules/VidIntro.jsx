ba8a845ba40e1c0730b1c8c0cb37758ef7c2315b
minjxx <qnsdbtlrp1018@naver.com>
2023-11-16 12:44:54 +0900

    update

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidIntro.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidIntro.jsx
@@ -1,29 +1,43 @@
 // DJ PJ 비디오소개 컴포넌트
 
+// 비디오소개 데이터 가져오기
+import { vidIntroData } from "../data/vid_intro";
+
+// 비디오소개 CSS 불러오기
+import "../../css/vid_intro.css"
+
 // 구조정의
-// Root > video box(>video+img) 
+// Root > section.vidbox > div
+// video box(>video+img)
 // + title box(>heading+p tag)
 
-export function VidIntro(){
+export function VidIntro(props) {
+  // props.cat - 페이지 분류명
+  // props.cls - 디자인적용 클래스('on')  
 
-  return(
-    <section className="vidbox">
-      {/* 1. 비디오파트 */}
-      <div className="vb1">
-        <iframe src="" title=""></iframe>
-      </div>
-      {/* 2. 타이틀/설명파트 */}
-      <div className="vb2">
-        {/* 작은제목 */}
-        <h3></h3>
-        {/* 큰제목 */}
-        <h2></h2>
-        {/* 요약소개 */}
-        <p></p>
-        {/* 설명(링크포함) */}
-        <p></p>
+  // 선택데이터 : 페이지 분류명으로 선택
+  const selData = vidIntroData[props.cat];
+
+  return (
+    /* props.cls로 전달되는 값이 'on'/'off'임 */
+    <section className={"vidbox"+' '+props.cls}>
+      <div>
+        {/* 1. 비디오파트 */}
+        <div className="vb1">
+          <iframe src={selData.vsrc} title={selData.btit}></iframe>
+        </div>
+        {/* 2. 타이틀/설명파트 */}
+        <div className="vb2">
+          {/* 작은제목 */}
+          <h3>{selData.stit}</h3>
+          {/* 큰제목 */}
+          <h2>{selData.btit}</h2>
+          {/* 요약소개 */}
+          <p>{selData.sum}</p>
+          {/* 설명(링크포함) */}
+          <p>{selData.desc}</p>
+        </div>
       </div>
     </section>
   );
-
-};
+}
