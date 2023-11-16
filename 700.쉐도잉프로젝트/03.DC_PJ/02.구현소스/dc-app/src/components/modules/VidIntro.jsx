5a795fce27bc83df8677399f6c78b4d9bee03c0e
minjxx <qnsdbtlrp1018@naver.com>
2023-11-16 14:37:51 +0900

    Update VidIntro.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidIntro.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidIntro.jsx
@@ -1,35 +1,34 @@
-// DJ PJ 비디오소개 컴포넌트
+// DC PJ 비디오소개 컴포넌트
 
 // 비디오소개 데이터 가져오기
 import { vidIntroData } from "../data/vid_intro";
 
 // 비디오소개 CSS 불러오기
-import "../../css/vid_intro.css"
-
-// 구조정의
-// Root > section.vidbox > div
-// video box(>video+img)
-// + title box(>heading+p tag)
+import "../../css/vid_intro.css";
+/* 구조정의: 
+Root> section.vidbox > div 
+> video box(>video+img) 
++ title box(>heading+p tag) */
 
 export function VidIntro(props) {
-  // props.cat - 페이지 분류명
-  // props.cls - 디자인적용 클래스('on')  
+    // props.cat - 페이지 분류명
+    // props.cls - 디자인적용 클래스('on')
 
-  // 선택데이터 : 페이지 분류명으로 선택
-  const selData = vidIntroData[props.cat];
+    // 선택데이터 : 페이지 분류명으로 선택
+    const selData = vidIntroData[props.cat];
 
-  // 링크생성함수 : desc 데이터 / sum 데이터에서 처리
-  const linkCode = (data) => {
-    return(
-      <>
-        {data.split('*')[0]}
-        <a href={selData.link[1]} target='_blank'>
-          {selData.link[0]}
-        </a>
-        {data.split('*')[1]}
-      </>
-    );
-  }; //////// linkCode 함수 /////////////
+    // 링크생성함수 : desc 데이터 / sum 데이터에서 처리
+    const linkCode = (data) => {
+        return(
+            <>
+                {data.split('*')[0]}
+                <a href={selData.link[1]} target='_blank'>
+                    {selData.link[0]}
+                </a>
+                {data.split('*')[1]}
+            </>
+        );
+    };///////// linkCode함수 ////////////
 
   return (
     /* props.cls로 전달되는 값이 'on'/'off'임 */
@@ -50,8 +49,8 @@ export function VidIntro(props) {
             {/* 특수문자(*)여부에 따라 처리
             indexOf(문자열) -> 없으면 -1리턴 */}
             {
-              selData.sum.indexOf('*') == -1 ? 
-              selData.sum : linkCode(selData.sum)
+                selData.sum.indexOf('*') == -1 ?
+                selData.sum : linkCode(selData.sum)                
             }
           </p>
           {/* 설명(링크포함) : desc */}
@@ -59,12 +58,12 @@ export function VidIntro(props) {
             {/* 특수문자(*)여부에 따라 처리
             indexOf(문자열) -> 없으면 -1리턴 */}
             {
-              selData.desc.indexOf('*') == -1 ? 
-              selData.desc : linkCode(selData.desc)
+                selData.desc.indexOf('*') == -1 ?
+                selData.desc : linkCode(selData.desc)                
             }
           </p>
         </div>
       </div>
     </section>
   );
-}
+} //////////// VidIntro 컴포넌트 ///////////
