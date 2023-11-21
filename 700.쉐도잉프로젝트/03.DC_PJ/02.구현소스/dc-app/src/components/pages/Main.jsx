9bd829fdb10318c4299284efd822c1d94740dd4d
minjxx <qnsdbtlrp1018@naver.com>
2023-11-21 15:27:11 +0900

    update

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
@@ -28,10 +28,13 @@ export function Main(){
             <VidIntro cat="main" cls="off" />
 
             {/* 4. 비디오스와이프 컴포넌트 */}
-            <VidSwipe tit="LATEST TRAILERS, CLIPS & MORE" />
+            <VidSwipe cat="main" />
 
-            {/* 5. 캐리터 리스트 컴포넌트 */}
+            {/* 5. 캐릭터 리스트 컴포넌트 */}
             <CatList />
+
+            {/* 6. 캐릭터 배너 컴포넌트 */}
+            <Banner category="CHARACTERS" />
         </>
     )
 
