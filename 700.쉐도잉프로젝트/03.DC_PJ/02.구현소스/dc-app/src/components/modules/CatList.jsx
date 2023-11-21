69f5c8710a95e0f309855a8e1f079293aba3a853
minjxx <qnsdbtlrp1018@naver.com>
2023-11-21 13:01:46 +0900

    update

---
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/CatList.jsx
@@ -0,0 +1,19 @@
+// DC PJ 캐릭터 리스트 컴포넌트
+
+// 캐릭터 리스트 모듈 CSS 불러오기
+import '../../css/cat_list.css';
+
+export function CatList(){
+
+  return(
+    <>
+      <section className="cat-swbox">
+        {/* 1. 모듈타이틀 */}
+        <h2 className="tit"></h2>
+        {/* 2. 스와이퍼 컴포넌트 */}
+        WHO'S WHO: THE JUSTICE LEAGUE
+      </section>
+    </>
+  )
+
+} /////// CatList 컴포넌트 ///////////
